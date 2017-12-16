8d76c832c017f3f2e0910089dd1474af381527f8
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2017-12-16 11:09:35 -0500

    on click of book pic logs data to the console

--- public/js/dashboardserver.js
+++ public/js/dashboardserver.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+  var url = "";
+  var img = "";
+  var title = "";
+  var isbn = "";
+  var author = "";
+
    $("#myform").submit(function(){
      modal.style.display = "none";
      $('#result').text("Please click on the image of the book to add it to your collection");
@@ -8,11 +14,6 @@ $(document).ready(function(){
    	  	alert("Please enter something in the field");
    	  }
    	  else{
-   	  var url = "";
-   	  var img = "";
-      var title = "";
-      var isbn = "";
-      var author = "";
      	  $.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
             for(i=0;i<response.items.length;i++)
             {
@@ -21,7 +22,10 @@ $(document).ready(function(){
              author=$('<h5 class="center-align white-text"> By:  ' + response.items[i].volumeInfo.authors + '</h5>');
              img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a target="_blank" href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>');
              url= response.items[i].volumeInfo.imageLinks.thumbnail;
-             img.attr({'src': url, 'data-title': title, 'data-author': author, 'data-isbn': isbn});
+             img.attr('src', url);
+             img.attr('data-title', response.items[i].volumeInfo.title);
+             img.attr('data-author', response.items[i].volumeInfo.authors);
+             img.attr('data-isbn', response.items[i].volumeInfo.industryIdentifiers[0].identifier);
              title.appendTo('#result');
              isbn.appendTo('#result')
              author.appendTo('#result');
@@ -32,3 +36,10 @@ $(document).ready(function(){
       return false;
    });
 });
+$(document).on("click", "img", logToDB);
+function logToDB () {
+  console.log($(this).attr("data-title"));
+  console.log($(this).attr("data-author"));
+  console.log($(this).attr("data-isbn"));
+
+}
