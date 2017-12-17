52e92644cf74c94c2fda692c10f6d0812d9466c7
Donald Joy <djsmacbookpro-2017@Donalds-MBP.fios-router.home>
2017-12-17 17:41:46 -0500

    dashboard is good to go

--- controllers/amazon.js
+++ controllers/amazon.js
@@ -1,5 +1,6 @@
+var bookToSell = "";
 
-  function amazonQuery() {
+  function amazonQuery(bookToSell) {
     var details = require('../details');
     const {OperationHelper} = require('apac');
     const opHelper = new OperationHelper({
@@ -16,18 +17,18 @@
       'Title': 'Stoic Warriors',
       'ResponseGroup': 'ItemAttributes, Offers, Images'
     }).then((response) => {
-      console.log("Results object: ", response.result);
-      console.log("***************************************************\n");
-      console.log("Amazon Page: ", response.result.ItemSearchResponse.Items.Item[0].DetailPageURL);
-      console.log("***************************************************\n");
+      // console.log("Results object: ", response.result);
+      // console.log("***************************************************\n");
+      // console.log("Amazon Page: ", response.result.ItemSearchResponse.Items.Item[0].DetailPageURL);
+      // console.log("***************************************************\n");
       // console.log("Item Attributes: ", response.result.ItemSearchResponse.Items.Item[0].ItemAttributes);
       // console.log("Lowest Used Price: ", response.result.ItemSearchResponse.Items.Item[0].OfferSummary.LowestUsedPrice.FormattedPrice);
-      console.log("***************************************************\n");
-      console.log("For Sale Info from Amazon: ", response.result.ItemSearchResponse.Items.Item[0].OfferSummary);
+      // console.log("***************************************************\n");
+      // console.log("For Sale Info from Amazon: ", response.result.ItemSearchResponse.Items.Item[0].OfferSummary);
 
         // console.log("Raw response body: ", response.responseBody);
     }).catch((err) => {
         console.error("Something went wrong! ", err);
     });
   };
-amazonQuery();
+module.exports = amazonQuery;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -61,7 +61,6 @@ module.exports = function(app) {
     });
   });
 
-
   app.get("/api/user_data", function(req, res) {
     if (!req.user) {
       res.json({});
